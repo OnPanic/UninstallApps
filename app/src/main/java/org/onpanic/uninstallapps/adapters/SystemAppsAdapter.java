7b897d75fb610c8ed821dc82976495349174b859
arrase <arrase@gmail.com>
2017-01-15 00:53:21 +0100

    more base code

---
+++ app/src/main/java/org/onpanic/uninstallapps/adapters/SystemAppsAdapter.java
@@ -0,0 +1,74 @@
+package org.onpanic.uninstallapps.adapters;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.onpanic.uninstallapps.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SystemAppsAdapter extends RecyclerView.Adapter<SystemAppsAdapter.viewHolder> {
+
+    private ArrayList<String> selectedApps;
+    private List<PackageInfo> availableApps;
+    private PackageManager mManager;
+    private Context mContext;
+
+    public SystemAppsAdapter(PackageManager pm, Context context) {
+        selectedApps = new ArrayList<>();
+        mManager = pm;
+        mContext = context;
+        availableApps = mManager.getInstalledPackages(0);
+    }
+
+    @Override
+    public SystemAppsAdapter.viewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.sys_app_layout, parent, false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final SystemAppsAdapter.viewHolder holder, int position) {
+        PackageInfo info = availableApps.get(position);
+        try {
+            holder.name.setText(mManager.getApplicationLabel(mManager.getApplicationInfo(info.packageName, 0)));
+            holder.icon.setImageDrawable(mManager.getApplicationIcon(info.packageName));
+        } catch (PackageManager.NameNotFoundException e) {
+            holder.name.setText(mContext.getString(R.string.invalid_package));
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return availableApps.size();
+    }
+
+    public ArrayList<String> getSelectedApps() {
+        return selectedApps;
+    }
+
+    class viewHolder extends RecyclerView.ViewHolder {
+
+        public final CheckBox selected;
+        public final ImageView icon;
+        public final TextView name;
+
+        viewHolder(final View row) {
+            super(row);
+            icon = (ImageView) row.findViewById(R.id.sys_app_icon);
+            name = (TextView) row.findViewById(R.id.sys_app_name);
+            selected = (CheckBox) row.findViewById(R.id.sys_app_selected);
+        }
+    }
+}
--- app/src/main/java/org/onpanic/uninstallapps/fragments/SystemAppsList.java
+++ app/src/main/java/org/onpanic/uninstallapps/fragments/SystemAppsList.java
@@ -1,14 +1,28 @@
 package org.onpanic.uninstallapps.fragments;
 
 import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import org.onpanic.uninstallapps.R;
+import org.onpanic.uninstallapps.adapters.SystemAppsAdapter;
+import org.onpanic.uninstallapps.providers.AppsProvider;
 
 public class SystemAppsList extends Fragment {
+    private Context mContext;
+    private PackageManager pm;
+    private SystemAppsAdapter mAdapter;
+    private Button cancel;
+    private Button save;
 
     public SystemAppsList() {
     }
@@ -16,6 +30,42 @@ public class SystemAppsList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_system_apps, container, false);
+        View v = inflater.inflate(R.layout.fragment_system_apps, container, false);
+
+        mAdapter = new SystemAppsAdapter(pm, mContext);
+        RecyclerView list = (RecyclerView) v.findViewById(R.id.apps_list);
+        list.setLayoutManager(new LinearLayoutManager(v.getContext()));
+        list.setAdapter(mAdapter);
+
+        cancel = (Button) v.findViewById(R.id.sys_app_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getFragmentManager().popBackStack();
+            }
+        });
+
+        save = (Button) v.findViewById(R.id.sys_app_save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentResolver cr = mContext.getContentResolver();
+                for (String app : mAdapter.getSelectedApps()) {
+                    ContentValues values = new ContentValues();
+                    values.put(AppsProvider.App.PACKAGE_NAME, app);
+                    cr.insert(AppsProvider.CONTENT_URI, values);
+                }
+                getActivity().getFragmentManager().popBackStack();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+        pm = mContext.getPackageManager();
     }
 }
